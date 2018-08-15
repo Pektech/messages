3fe233f2cafc6884344e4efc791ccc7e813a84ab
Ric Leddy <14008446+Pektech@users.noreply.github.com>
2018-08-14 21:16:56 -0400

    much simpler message table with added proxy

--- app/models.py
+++ app/models.py
@@ -1,17 +1,16 @@
 from app import db
+from sqlalchemy.ext.associationproxy import association_proxy
 
 
 
-family_mess = db.Table('family_mess',
-                db.Column('family_id', db.Integer, db.ForeignKey('family.id')),
-                db.Column('messages_id', db.Integer, db.ForeignKey('messages.id')))
+
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     alexa_id = db.Column(db.String(230), unique=True, nullable=False)
 
-    family = db.relationship('Family', backref='user', lazy=True)
+    family_members = db.relationship('Family', lazy=True)
 
     def __repr__(self):
         return '<Alexa {}>'.format(self.alexa_id)
@@ -20,7 +19,11 @@ class Family(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     name = db.Column(db.String(120))
-    messages = db.relationship('Messages', backref='family', lazy=True)
+    mess_to = db.relationship('Messages',foreign_keys='Messages.to_id',
+                               backref='fam_sent', lazy=True)
+    mess_from = db.relationship('Messages',foreign_keys='Messages.from_id',
+                               backref='fam_to', lazy=True)
+    show = association_proxy('mess_to', 'message')
 
     def __repr__(self):
         return '<member : {}>'.format(self.name)
@@ -28,12 +31,10 @@ class Family(db.Model):
 
 class Messages(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    family_id = db.Column(db.Integer, db.ForeignKey('family.id'))
+    from_id = db.Column(db.Integer, db.ForeignKey('family.id'))
     message = db.Column(db.Text(280))
-    family_mess_table = db.relationship(
-        'Messages', secondary=family_mess,
-        primaryjoin=(family_mess.c.family_id==id),
-        secondaryjoin=(family_mess.c.messages_id==id),
-        backref=db.backref('family_mess', lazy='dynamic'), lazy='dynamic')
+    to_id = db.Column(db.Integer,db.ForeignKey('family.id'))
+    show = association_proxy('messages','message')
+
 
 
--- messages.py
+++ messages.py
@@ -1,10 +1,10 @@
 from app import app, db
-from app.models import User, Family, Messages, family_mess
+from app.models import User, Family, Messages
 
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Family': Family, 'Messages': Messages,
-            'family_mess': family_mess}
+            }
 
